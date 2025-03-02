d4017ac47ebfcfa7239ff6219feadf771be02fcc
jonsduname <johnchun90@gmail.com>
2025-03-01 17:21:22 -0800

    api call, displayed image for the correct color scheme

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import Header from './Header'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedScheme, setSelectedScheme] = useState('')
+  const [color, setColor] = useState('000000')
+  const [colorData, setColorData] = useState()
+
+  const handleColorChange = e => {
+    setColor(e.target.value.slice(1,7))
+    
+  }
+
+
+  console.log(color)
+  console.log(selectedScheme)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     <Header color={color} handleColorChange={handleColorChange} selectedScheme={selectedScheme} setSelectedScheme={setSelectedScheme} colorData={colorData} setColorData={setColorData}/>
+     {colorData && <img className="scheme-image"src={colorData.image.bare} />}
+     <div className='scheme-hex-values'>
+      {colorData && colorData.colors.map(color => {
+        return (
+        <p>{color.hex.value}</p>
+        )
+      })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/Header.jsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react"
+export default function Header(props) {
+    const { color, handleColorChange, selectedScheme, setSelectedScheme, colorData, setColorData} = props
+
+    function handleSubmit() {
+        fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${selectedScheme}`)
+            .then(res => {
+              if(!res.ok) {
+                throw new Error(console.log(error))
+              }
+              return res.json()
+            })
+            .then(data => setColorData(data))
+          console.log(colorData)
+      }
+    return (
+        <div>
+             <input type='color' value={color} onChange={handleColorChange}/>
+            <select value={selectedScheme} onChange={e => setSelectedScheme(e.target.value)} >
+                <option value="monochrome">monochrome</option>
+                <option value="monochrome-dark">monochrome-dark</option>
+                <option value="monochrome-light">monochrome-light</option>
+                <option value="analogic">analogic</option>
+                <option value="complement">complement</option>
+                <option value="analogic-complement">analogic-complement</option>
+                <option value="triad">triad</option>
+                <option value="quad">quad</option>
+            </select>
+            <button onClick={handleSubmit}>Get Color Scheme</button>
+        </div>
+    )
+}
